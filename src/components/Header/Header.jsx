b39f697426dc0a473b3cae6c0b687d168ee8b0e1
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-29 12:36:08 +0600

    Setup Enviroment and implement the header section without responsive

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+  return (
+    <header className='bg-linear-white'>
+      <nav className='container header-nav'>
+        <div className="brand-name fw-800 fs-32 font-dark-1">
+          CareerHub
+        </div>
+        <div className='header-links'>
+          <Link to='/home'  className='font-dark-03 fs-16 fw-500'>Home</Link>
+          <Link to='/apply'  className='font-dark-03 fs-16 fw-500'>Applied Job</Link>
+          <Link to='/blog'  className='font-dark-03 fs-16 fw-500'>Blog</Link>
+        </div>
+        <div className='header-btn'>
+          <button className='primary-btn'><Link>Star Applying</Link></button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/Home.jsx
@@ -0,0 +1,15 @@
+import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
