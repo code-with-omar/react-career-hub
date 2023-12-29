065f1895e6d97d42758d9f793ce0714165dc22b5
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-29 22:01:20 +0600

    Implement hero section without responsive

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+
+import Hero from '../Hero/Hero';
+
+const Main = () => {
+    return (
+        <main className='container'>
+            <Hero></Hero>
+        </main>
+    );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter, RouterProvider,
 } from "react-router-dom";
 import Home from './components/Layout/Home.jsx';
+import Hero from './components/Hero/Hero.jsx';
+import Main from './components/Main/Main.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    children:[
+      {
+        path:'/',
+        element:<Main></Main>
+      },
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
