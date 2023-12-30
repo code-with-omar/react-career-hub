4acbfe0721b7a515cdcfbea61ba1eea2699e39e5
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-31 02:45:13 +0600

    Apply btn implement and aslo use functionity

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -5,13 +5,18 @@ import moneyIcon from '../../assets/icons/money.png';
 import locationIcon from '../../assets/icons/location2.png'
 import jobIcon from '../../assets/icons/calendar.png';
 import './JobDetails.css'
+import { saveJobApplication } from '../../../utilities/localstorage';
 const JobDetails = () => {
     const jobs = useLoaderData();
     const { id } = useParams();
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
     const { job_description, job_responsibility, educational_requirements, experiences, salary, job_title, contact_information } = job;
-    console.log(contact_information.email)
+    
+    // apply fuctionility
+    const handleApply=(job)=>{
+        saveJobApplication(job.id)
+    }
     return (
         <section className='job-details-wrap'>
             <div className='bg-linear-white job-details-title'>
@@ -27,36 +32,40 @@ const JobDetails = () => {
                         <p className='fs-16 fw-800 font-dark-01'>Experiences:</p>
                         <p>{experiences}</p>
                     </div>
-                    <div className='job-details-right '>
-                        <h4 className='fs-20 font-dark-01 fw-800 '>Job Details</h4>
-                        <hr className='hr' />
-                        <div>
-                            <div className='job-details-right-section-01'>
-                                <img src={moneyIcon} alt="" />
-                                <h5 className='fs-18 font-dark-01 fw-700'>Salary: <span className='font-dark-03 fw-600'>{salary}</span></h5>
+                    <div className='job-details-right'>
+                        <div className='job-details-right-contain '>
+                            <h4 className='fs-20 font-dark-01 fw-800 '>Job Details</h4>
+                            <hr className='hr' />
+                            <div>
+                                <div className='job-details-right-section-01'>
+                                    <img src={moneyIcon} alt="" />
+                                    <h5 className='fs-18 font-dark-01 fw-700'>Salary: <span className='font-dark-03 fw-600'>{salary}</span></h5>
+                                </div>
+                                <div className='job-details-right-section-01'>
+                                    <img src={jobIcon} alt="" />
+                                    <h5 className='fs-18 font-dark-01 fw-807'>Job Title: <span className='font-dark-03 fw-600'>{job_title}</span> </h5>
+                                </div>
                             </div>
-                            <div className='job-details-right-section-01'>
-                                <img src={jobIcon} alt="" />
-                                <h5 className='fs-18 font-dark-01 fw-807'>Job Title: <span className='font-dark-03 fw-600'>{job_title}</span> </h5>
+                            <h4 className='fs-20 font-dark-01 fw-800 '>Contact Information</h4>
+                            <hr className='hr' />
+                            <div>
+                                <div className='job-details-right-section-01'>
+                                    <img src={phoneIcon} alt="" />
+                                    <h5 className='fs-18 font-dark-01 fw-700'>Phone:<span className='font-dark-03 fw-600'>{contact_information.phone}</span> </h5>
+                                </div>
+                                <div className='job-details-right-section-01'>
+                                    <img src={emailIcon} alt="" />
+                                    <h5 className='fs-18 font-dark-01 fw-700'>email: <span className='font-dark-03 fw-600'>{contact_information.email}</span> </h5>
+                                </div>
+                                <div className='job-details-right-section-01'>
+                                    <img src={locationIcon} alt="" />
+                                    <h5 className='fs-18 font-dark-01 7w-800'>Address: <span className='font-dark-03 fw-600'> {contact_information.address}</span></h5>
+                                </div>
                             </div>
                         </div>
-                        <h4 className='fs-20 font-dark-01 fw-800 '>Contact Information</h4>
-                        <hr className='hr' />
-                        <div>
-                            <div className='job-details-right-section-01'>
-                                <img src={phoneIcon} alt="" />
-                                <h5 className='fs-18 font-dark-01 fw-700'>Phone:<span className='font-dark-03 fw-600'>{contact_information.phone}</span> </h5>
-                            </div>
-                            <div className='job-details-right-section-01'>
-                                <img src={emailIcon} alt="" />
-                                <h5 className='fs-18 font-dark-01 fw-700'>email: <span className='font-dark-03 fw-600'>{contact_information.email}</span> </h5>
-                            </div>
-                            <div className='job-details-right-section-01'>
-                                <img src={locationIcon} alt="" />
-                                <h5 className='fs-18 font-dark-01 7w-800'>Address: <span className='font-dark-03 fw-600'> {contact_information.address}</span></h5>
-                            </div>
+                        <div className='job-apply-btn'>
+                            <button className ='primary-btn'onClick={() => handleApply(job)}>Apply Now</button>
                         </div>
-                        
                     </div>
                 </div>
             </div>
